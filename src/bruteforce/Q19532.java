c3d301d7b880bc638a07bc3a1b2cb001fd5ace1a
dvlp-sy <sy8389b@gmail.com>
2026-04-27 14:45:59 +0900

    Create Q19532.java

---
+++ src/bruteforce/Q19532.java
@@ -0,0 +1,31 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q19532 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for (int x = -999 ; x <= 999 ; x++) {
+            for (int y = -999 ; y <= 999 ; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.println(x + " " + y);
+                    return;
+                }
+            }
+        }
+    }
+}
