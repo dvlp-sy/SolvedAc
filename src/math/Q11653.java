615169730d26d7d28e9cd11c13cde76252dcea9e
dvlp-sy <sy8389b@gmail.com>
2026-04-28 15:51:56 +0900

    Create Q11653.java

---
+++ src/math/Q11653.java
@@ -0,0 +1,24 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11653 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        while (N != 1) {
+            for (int i = 2; i <= N ; i++) {
+                if (N % i == 0) {
+                    System.out.println(i);
+                    N /= i;
+                    break;
+                }
+            }
+        }
+    }
+}
